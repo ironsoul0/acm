1fc159bcbd5e5f310f4801f3ccfada515647f572
ironsoul <timka2609@gmail.com>
2019-09-21 17:32:58 +0600

    New solutions

--- Tryouts/E/main.cpp
+++ Tryouts/E/main.cpp
@@ -16,11 +16,11 @@
 
 using namespace std;
 
-typedef long long ll;
+typedef unsigned long long ll;
 typedef pair<int, int> pii;
 
 const int MOD = (int) 1e9 + 7;
-const int N = (int) 1e6 + 7;
+const int N = (int) 31;
 const ll INF = (ll) 1e18 + 7;
 
 int main() {
@@ -29,6 +29,30 @@ int main() {
 		freopen("input.txt", "r", stdin);
 		freopen("output.txt", "w", stdout);
 	#endif
-	
+	int n;
+	cin >> n;
+	vector<int> l(n);
+	for (int i = 0; i < n; i++) {
+		cin >> l[i];
+	}
+	for (int k = 2; k <= n; k++) {
+		if (n % k) continue;
+		int divs = n / k;
+		unordered_map<int, int> rems;
+		for (auto x : l) {
+			rems[x % k]++;
+		}
+		bool valid = true;
+		for (int i = 0; i < k; i++) {
+			if (rems[i] != divs) {
+				valid = false;
+			}
+		}
+		if (valid) {
+			cout << k;
+			return 0;
+		}
+	}
+	cout << -1;
 	return 0;
 }
--- Tryouts/I/main.cpp
+++ Tryouts/I/main.cpp
@@ -23,12 +23,35 @@ const int MOD = (int) 1e9 + 7;
 const int N = (int) 1e6 + 7;
 const ll INF = (ll) 1e18 + 7;
 
+void hanoy(int val, char start, char buff, char end) {
+	if (val == 0) return;
+	hanoy(val - 1, start, end, buff);
+	cout << start << ' ' << end << '\n';
+	hanoy(val - 1, buff, start, end);
+}
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	#ifdef LOCAL
 		freopen("input.txt", "r", stdin);
 		freopen("output.txt", "w", stdout);
 	#endif
-	
+	int n, k;
+	cin >> n >> k;
+	int pw = (1 << n) - 1;
+	if (k < pw || k == pw + 1) {
+		cout << "N";
+	} else {
+		cout << "Y\n";
+		int diff = k - pw;
+		if (diff & 1) {
+			cout << "A B\nB C\nC A\n";
+			diff -= 3; 
+		}
+		for (int i = 0; i < diff / 2; i++) {
+			cout << "A B\nB A\n";
+		}
+		hanoy(n, 'A', 'B', 'C');
+	}
 	return 0;
 }
